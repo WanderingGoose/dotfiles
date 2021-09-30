5d71eda1fd3ead5904412f65dc9f91e74f7dd08b
Alec Whittington <alec.whittington@gmail.com>
2021-09-29 17:05:37 -0700

    fixing volta install

--- volta/install.sh
+++ volta/install.sh
@@ -1,9 +1,9 @@
-install-nodejs() {
-    install-volta() {
+install-nodejs () {
+    install-volta () {
         sh -c "$(curl -fsSL https://get.volta.sh)"  -- -y
     }
     
-    install-node() {
+    install-node () {
         echo "-----------------------------------------------------------"
         echo "* Installing Node 12 and 14"
         echo "-----------------------------------------------------------"
@@ -11,11 +11,11 @@ install-nodejs() {
         volta install node@14
     }
 
-    install-yarn() {
+    install-yarn () {
         volta install yarn@1
     }
 
-    yarn-global-add() {
+    yarn-global-add () {
         echo "-----------------------------------------------------------"
         echo "* Yarn Global Add those packages:"
         echo "-----------------------------------------------------------"
