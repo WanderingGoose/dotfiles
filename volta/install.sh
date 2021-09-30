d63c9cc055918157d13120a3576254f6253fc260
Alec Whittington <alec.whittington@gmail.com>
2021-09-29 17:13:46 -0700

    Working on installed

--- volta/install.sh
+++ volta/install.sh
@@ -1,35 +1,42 @@
-install-nodejs () {
-    install-volta () {
-        sh -c "$(curl -fsSL https://get.volta.sh)"  -- -y
-    }
-    
-    install-node () {
-        echo "-----------------------------------------------------------"
-        echo "* Installing Node 12 and 14"
-        echo "-----------------------------------------------------------"
-        volta install node@12
-        volta install node@14
-    }
-
-    install-yarn () {
-        volta install yarn@1
-    }
-
-    yarn-global-add () {
-        echo "-----------------------------------------------------------"
-        echo "* Yarn Global Add those packages:"
-        echo "-----------------------------------------------------------"
-        yarn global add nx@latest
-    }
-
-
-    echo "==========================================================="
-    echo "              Setting up NodeJS Environment"
-
-    install-volta
-    install-node
-    install-yarn
-    yarn-global-add
+install_volta () {
+    sh -c "$(curl -fsSL https://get.volta.sh)"  -- -y
 }
 
-install-nodejs
+install_node () {
+    echo "-----------------------------------------------------------"
+    echo "* Installing Node 12 and 14"
+    echo "-----------------------------------------------------------"
+    volta install node@12
+    volta install node@14
+}
+
+install_yarn () {
+    volta install yarn@1
+}
+
+yarn_global_add () {
+    echo "-----------------------------------------------------------"
+    echo "* Yarn Global Add those packages:"
+    echo "-----------------------------------------------------------"
+    yarn global add nx@latest
+}
+
+
+echo "==========================================================="
+echo "              Setting up NodeJS Environment"
+
+if test ! $(which volta)
+then
+    echo "  Installing Volta for you."
+
+    install_volta
+    install_node
+    install_yarn
+    yarn_global_add
+
+    echo " Volta, Node, and Yarn installed"
+else
+    echo " Volta already installed"
+fi
+
+exit 0
