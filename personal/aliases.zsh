4cf78a396807ebb5b8ae7e92c5d2599f449dbc3e
Alec Whittington <alec.whittington@gmail.com>
2022-02-08 22:29:02 -0700

    Updated personal aliases.zsh

--- personal/aliases.zsh
+++ personal/aliases.zsh
@@ -9,9 +9,10 @@ alias bomb="nx serve buildomatic"
 alias cole="cd ~/source/personal/coleman"
 alias coler="cd ~/source/personal/coleman/rust/coleman"
 alias sms="cd ~/source/personal/scrum-master-studio"
-alias sms-code="sms && cd ./sms && code ."
+alias smss="sms && cd ./sms"
+alias sms-code="smss && code ."
 
 ### Open Source ###
 alias salam="cd ~/source/work/agile-master"
-alias salams="cd ~/source/work/agile-master/agile-master"
+alias salams="salam && cd agile-master"
 alias salam-code="cd salams && code ."
