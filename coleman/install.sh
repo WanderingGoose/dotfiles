73c6ee35d58620c11875dd0f6cf5e62827b41982
Head Ninja <alec@fatninjas.work>
2022-06-15 15:18:00 -0700

    Fixed install scripts Added font install script

--- coleman/install.sh
+++ coleman/install.sh
@@ -14,8 +14,14 @@ if test ! $(which coleman); then
     echo "        Coleman source directory already exists"
   else
     echo "           creating Coleman source directory"
-    cd "$HOME/source/personal"
-    git@github.com:alecwhittington/coleman.git
+    if [ -d "$HOME/source/personal" ]; then
+      cd "$HOME/source/personal"
+    else 
+      mkdir -p "$HOME/source/personal"
+      cd "$HOME/source/personal"
+    fi
+    
+    git clone git@github.com:alecwhittington/coleman.git
     echo "        Coleman source directory created"
   fi
 
---
+++ fonts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+brew install --cask font-fira-code-nerd-font
+brew install --cask font-fira-mono-nerd-font
