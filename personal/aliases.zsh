acb480b36a680f473e87fb2cd34872f0c27c6ffb
Head Ninja <alec@fatninjas.work>
2022-07-17 14:13:19 -0700

    Updated some aliases

--- personal/aliases.zsh
+++ personal/aliases.zsh
@@ -8,14 +8,16 @@ alias dcps="docker-compose ps"
 alias dprune="docker system prune --all --force --volumes"
 
 ### Fat Ninja's Work ###
-alias fnw="cd ~/source/personal/fnw/fnw"
+alias fnw="cd ~/source/fnw/fnw"
 alias fnw-code="fnw && code ."
-alias bomb="nx serve buildomatic"
+alias bomb="yarn nx serve buildomatic"
 alias cole="cd ~/source/personal/coleman"
-alias coler="cd ~/source/personal/coleman/rust/coleman"
-alias dds="cd ~/source/3rd-party/duck-duck-scrum"
+alias coler="cd ~/source/coleman/rust/coleman"
+alias dds="cd ~/source/duck-duck-scrum"
 alias ddss="dds && cd ./dds"
 alias ddsc="ddss && vsc"
+alias fw="cd ~/source/fonzie-works/fw"
+alias fw=c"fw && code ."
 
 ### Open Source ###
 alias sms="cd ~/source/3rd-party/scrum-master-studio"
