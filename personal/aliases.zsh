42ccf7edd55231bb215ed4eaa8abf58aaa3d1ebd
Head Ninja <alec@fatninjas.work>
2023-01-15 16:17:16 -0700

    Updated line endings

--- personal/aliases.zsh
+++ personal/aliases.zsh
@@ -1,12 +1,14 @@
 ### Personal ###
 alias df="cd ~/.dotfiles"
 alias dfcode="df && code ."
+alias dfpull="df && git pull"
 
 ### Docker
 alias dcd="docker-compose down"
 alias dcps="docker-compose ps"
 alias dprune="docker system prune --all --force --volumes"
 alias dcfnw="docker compose up --build -d"
+alias dcfnwdown="docker compose down"
 
 ### Fat Ninja's Work ###
 alias fnw="cd ~/source/fnw/Frontend"
