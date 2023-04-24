318016e5dbc7a6cc063506b6c571c107b25a7144
Goose <alec@fatninjas.work>
2023-04-24 08:58:37 -0700

    Added aliases for Python and PIP

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 # ALIASES #
 ###########
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Python #
+alias py=/Users/head-ninja/.pyenv/versions/3.11.3/bin/python3.11
+alias python=/Users/head-ninja/.pyenv/versions/3.11.3/bin/python3.11
+alias pip=/Users/head-ninja/.pyenv/versions/3.11.3/bin/pip3.11
