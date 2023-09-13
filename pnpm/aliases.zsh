692756d020632ef31321f6a7f7e8ebdf9018db43
Head Ninja <alec@fatninjas.work>
2023-09-13 16:53:12 -0700

    Add pnpm aliases and environment variables
    - Added aliases for project operations in the pnpm/aliases.zsh file.
    - Updated the zsh/zshrc.symlink file to include environment variables for pnpm.
    - Ignored the pnpm-lock.yaml file.
    
    The changes include adding aliases for common pnpm commands such as build, start, serve, and test. Additionally, environment variables were added to set the PNPM_HOME path and update the PATH variable accordingly. The commit also ignores the pnpm-lock.yaml file.

--- pnpm/aliases.zsh
+++ pnpm/aliases.zsh
@@ -3,3 +3,10 @@ alias pa="pnpm add"
 alias pi="pnpm install"
 alias pr="pnpm remove"
 alias pup="pnpm update"
+
+## Project Operations
+alias @pb="pnpm build"
+alias @pbsc="pnpm build:no-cache"
+alias @ps="pnpm start"
+alias @psv="pnpm serve"
+alias @pt="pnpm test"
