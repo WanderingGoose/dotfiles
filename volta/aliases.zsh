02aa57563261e7a13b7878d3cd35092ed0328f53
Head Ninja <alec@fatninjas.work>
2023-10-04 13:19:39 -0700

    Update Node and Yarn aliases
    - Updated Node aliases to include version 20
    - Added new Node alias for version 20
    - Updated Node pinning aliases to include version 20
    - Added new Node pinning alias for version 20
    - Updated Yarn alias to install version 1

--- volta/aliases.zsh
+++ volta/aliases.zsh
@@ -1,10 +1,9 @@
-alias vin12="volta install node@12"
-alias vin14="volta install node@14"
-alias vin16="volta install node@16"
+# Node
 alias vin18="volta install node@18"
-alias viy1="volta install yarn@1"
-alias vpn12="volta pin node@12"
-alias vpn14="volta pin node@14"
-alias vpn16="volta pin node@16"
+alias vin20="volta install node@20"
 alias vpn18="volta pin node@18"
+alias vpn20="volta pin node@20"
+
+# Yarn
+alias viy1="volta install yarn@1"
 alias vpy1="volta pin yarn@1"
