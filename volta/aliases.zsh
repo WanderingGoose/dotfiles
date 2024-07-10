c96403c317ae84fdb8004147eb1ae94d9230670f
Head Ninja <alec@fatninjas.work>
2024-07-10 12:42:04 -0700

    Added new aliases for pnpm and volta
    In this update, we've introduced a few more handy shortcuts to improve your workflow. For pnpm, a new alias has been added to facilitate development tasks. On the volta side, we've expanded the node version management with additional aliases for installing and pinning Node.js version 22. Also, an alias was added for installing pnpm via Volta.

--- volta/aliases.zsh
+++ volta/aliases.zsh
@@ -1,9 +1,14 @@
 # Node
 alias vin18="volta install node@18"
 alias vin20="volta install node@20"
+alias vin22="volta install node@22"
 alias vpn18="volta pin node@18"
 alias vpn20="volta pin node@20"
+alias vpn22="volta pin node@22"
 
 # Yarn
 alias viy1="volta install yarn@1"
 alias vpy1="volta pin yarn@1"
+
+#pnpm
+alias vip="volta install pnpm"
